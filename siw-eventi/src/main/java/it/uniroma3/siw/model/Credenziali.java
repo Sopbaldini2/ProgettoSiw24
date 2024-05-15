581451b07bcaa333ed5b618bfc855cc066ca4aae
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2024-05-15 10:52:58 +0200

    scrittura prime classi

---
+++ siw-eventi/src/main/java/it/uniroma3/siw/model/Credenziali.java
@@ -0,0 +1,66 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Credenziali {
+
+	public static final String DEFAULT_ROLE = "DEFAULT";
+	public static final String ADMIN_ROLE = "ADMIN";
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	private String username;
+	private String password;
+	private String ruolo;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Cliente cliente;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getRuolo() {
+		return ruolo;
+	}
+	
+	public void setRole(String ruolo) {
+		this.ruolo = ruolo;
+	}
+
+}
